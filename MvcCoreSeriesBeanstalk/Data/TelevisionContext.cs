fefa012006a2dc3e152c0db183845038efbf20e9
hmar321 <hmauricio.ar@gmail.com>
2024-05-21 11:48:14 +0200

    Add project files.

---
+++ MvcCoreSeriesBeanstalk/Data/TelevisionContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreSeriesBeanstalk.Models;
+
+namespace MvcCoreSeriesBeanstalk.Data
+{
+    public class TelevisionContext : DbContext
+    {
+        public TelevisionContext(DbContextOptions<TelevisionContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Serie> Series { get; set; }
+    }
+}
